8dff8e649fe86d0a713a8a69b85242f6bc3ca7bb
Gabriel_Chan <gabriel.chanyk@gmail.com>
2020-09-23 20:01:26 -0400

    first commit

---
+++ InventoryApp.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Gabriel Chan
+Date: December 6,2018
+Title: Inventory Control System
+Description: This program will help your business keep track of your stock, profit, overall profit and the placement of orders
+Version:1.0
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(void)
+{
+    return (0);
+}
